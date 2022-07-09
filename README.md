427e386591b55a401b413de07726ecd9669086d4
BooneDev <noahcaldwell36@gmail.com>
2022-07-09 00:02:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Facebook Market Sniper
+
+### A program designed to monitor facebook marketplace for item thats meet a specified criteria such as price and item condition.
+#
+<img src="https://ai.github.io/size-limit/logo.svg" align="right"
+     alt="Size Limit logo by Anton Lovchikov" width="120" height="178">
+
---
+++ scrape_facebook.py
@@ -0,0 +1,24 @@
+"""
+
+This file can be used to retrieve data from the Facebook Marketplace using 
+keywords and price criteria.
+
+"""
+
+def get_market_data(price, search_words, req_words):
+    pass
+
+    # go to facebook
+
+    # login to facebook (skip this step if possible)
+    
+    # open facebook marketplace
+    
+    # search the keywords 
+
+    # get page results
+    
+    # filter results based on requirements
+    
+    # return list of results
+
